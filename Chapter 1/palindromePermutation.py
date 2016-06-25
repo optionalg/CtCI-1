461c04842b785ac4377a8fcf1ea7c4652d3af26e
Torry Yang <torryyang@gmail.com>
2016-06-25 22:52:26 +0800

    solved palindromePerumutation

--- Chapter 1/palindromePermutation.py
+++ Chapter 1/palindromePermutation.py
@@ -1,1 +1,28 @@
 import unittest
+
+def palinPerm(s):
+	s = s.replace(' ','').lower()
+	count = {}
+
+	for let in s:
+		if let in count:
+			count[let] += 1
+		else:
+			count[let] = 1
+
+	oddCount = 0
+	for x in count:
+		if count[x] % 2 == 1:
+			oddCount += 1
+
+	return oddCount < 2
+
+class Test(unittest.TestCase):
+	def test(self):
+		self.assertTrue(palinPerm('Tact Coa'))
+		self.assertTrue(palinPerm('Able was I ere I saw Elba'))
+		self.assertFalse(palinPerm('So patient a nurse to nurse a patient so'))
+		self.assertFalse(palinPerm('Random Words'))
+
+if __name__ == "__main__":
+	unittest.main()
