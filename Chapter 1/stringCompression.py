567ead95a5a417dd33a23808bbaa1e5be6100ea0
Torry Yang <torryyang@gmail.com>
2016-07-09 23:54:43 +0800

    finished stringCompression

--- Chapter 1/stringCompression.py
+++ Chapter 1/stringCompression.py
@@ -1,7 +1,24 @@
 import unittest
 
+def stringCompression(string):
+	compressed = []
+	counter = 0
+
+	for x in range(len(string)):
+		if x != 0 and string[x] != string[x-1]:
+			compressed.append(string[x-1] + str(counter))
+			counter = 0
+		counter += 1
+
+	compressed.append(string[-1] + str(counter))
+
+	return min(string, ''.join(compressed), key=len)
+
+
 class Test(unittest.TestCase):
 	def test(self):
+		self.assertEqual(stringCompression('aabcccccaaa'),'a2b1c5a3')
+		self.assertEqual(stringCompression('abcdef'),'abcdef')		
 
 if __name__ == "__main__":
 	unittest.main()
