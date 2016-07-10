47fb79f59111ee432c11e7e694051a63d664fa8e
Torry Yang <torryyang@gmail.com>
2016-07-10 15:42:49 +0800

    finish stringRotation

--- Chapter 1/stringRotation.py
+++ Chapter 1/stringRotation.py
@@ -1,7 +1,20 @@
 import unittest
 
+def isSubstring(s,sub):
+	return sub in s
+
+def stringRotation(s1,s2):
+	if len(s1) == len(s2) != 0:
+		return isSubstring(s1 + s1, s2)
+	return False
+
+
 class Test(unittest.TestCase):
 	def test(self):
+		self.assertTrue(stringRotation('erbottlewat','waterbottle'))
+		self.assertFalse(stringRotation('watermelon','mewaterlon'))
+		self.assertTrue(stringRotation('watermelon','melonwater'))
+		self.assertTrue(stringRotation('melonwater','watermelon'))
 
 if __name__ == "__main__":
 	unittest.main()
